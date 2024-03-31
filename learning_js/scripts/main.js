e7c5df8bfc613e7a795a9bc7b1283f29d6b6f4ee
Michael Foreman <mike.seyfertssextet@gmail.com>
2024-03-31 15:39:44 -0400

    Add test button callback

--- learning_js/scripts/main.js
+++ learning_js/scripts/main.js
@@ -1,2 +1,32 @@
+function changeTextInDemo() {
+    let demoDefaultText = "Change this text with the button below!"
+    let demoAltText = "Hello JavaScript!"
+
+    let demoHandle = document.getElementById("demo");
+    let demoText = demoHandle.innerHTML;
+
+    if (demoText === demoDefaultText) {
+        demoText = demoAltText;
+    } else {
+        demoText = demoDefaultText;
+    }
+}
+
+const script_button = document.getElementById("script_button");
+// script_button.addEventListener("click", changeTextInDemo);
+script_button.addEventListener("click", function() {
+    let demoDefaultText = "Change this text with the button below!"
+    let demoAltText = "Hello JavaScript!"
+
+    let demoHandle = document.getElementById("demo");
+    let demoText = demoHandle.innerHTML;
+
+    if (demoText === demoDefaultText) {
+        demoText = demoAltText;
+    } else {
+        demoText = demoDefaultText;
+    }
+});
+
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello JavaScript World!";
