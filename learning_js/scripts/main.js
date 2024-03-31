46bdc7584435429b960fb0450c1e94683e3b8731
Michael Foreman <mike.seyfertssextet@gmail.com>
2024-03-31 16:04:26 -0400

    Test button callback attempt 4

--- learning_js/scripts/main.js
+++ learning_js/scripts/main.js
@@ -1,24 +1,4 @@
 function changeTextInDemo() {
-    let demoDefaultText = "Change this text with the button below!"
-    let demoAltText = "Hello JavaScript!"
-
-    let demoHandle = document.getElementById("demo");
-    let demoText = demoHandle.innerHTML;
-
-    if (demoText === demoDefaultText) {
-        demoText = demoAltText;
-    } else {
-        demoText = demoDefaultText;
-    }
-}
-
-// Looking at https://stackoverflow.com/a/1947531/6186275
-
-// function pointer
-// document.getElementById("script_button").addEventListener("click", changeTextInDemo);
-
-// anonymous function
-document.getElementById("script_button").addEventListener("click", function() {
     // let demoDefaultText = "Change this text with the button below!"
     // let demoAltText = "Hello JavaScript!"
 
@@ -32,7 +12,29 @@ document.getElementById("script_button").addEventListener("click", function() {
     // }
 
     document.getElementById("demo").innerHTML = "Hello JavaScript!";
-});
+}
+
+// Looking at https://stackoverflow.com/a/1947531/6186275
+
+// function pointer
+// document.getElementById("script_button").addEventListener("click", changeTextInDemo);
+
+// anonymous function
+// document.getElementById("script_button").addEventListener("click", function() {
+//     // let demoDefaultText = "Change this text with the button below!"
+//     // let demoAltText = "Hello JavaScript!"
+
+//     // let demoHandle = document.getElementById("demo");
+//     // let demoText = demoHandle.innerHTML;
+
+//     // if (demoText === demoDefaultText) {
+//     //     demoText = demoAltText;
+//     // } else {
+//     //     demoText = demoDefaultText;
+//     // }
+
+//     document.getElementById("demo").innerHTML = "Hello JavaScript!";
+// });
 
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello JavaScript World!";
