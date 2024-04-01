b63ecc031f08a76889bf15e9460debafa2d6023f
Michael Foreman <mike.seyfertssextet@gmail.com>
2024-03-31 22:39:20 -0400

    Fix whitespace issue with text-changing button

--- learning_js/scripts/main.js
+++ learning_js/scripts/main.js
@@ -1,15 +1,17 @@
 function changeTextInDemo() {
-    var demoDefaultText = "Change this text with the button below!"
-    var demoAltText = "Hello JavaScript!"
+    var demoDefaultText = "Change this text with the button below!";
+    var demoAltText = "Hello JavaScript!";
 
     var demoHandle = document.getElementById("demo");
-    var demoText = demoHandle.innerHTML;
+    var demoText = demoHandle.innerHTML.trim();
+
+    console.log("Text is currently \"" + demoText + "\"");
 
     if (demoText === demoDefaultText) {
-        console.log("Changing text to " + demoAltText);
+        console.log("Changing text to \"" + demoAltText + "\"");
         demoHandle.innerHTML = demoAltText;
     } else {
-        console.log("Changing text to " + demoDefaultText);
+        console.log("Changing text to \"" + demoDefaultText + "\"");
         demoHandle.innerHTML = demoDefaultText;
     }
 }
